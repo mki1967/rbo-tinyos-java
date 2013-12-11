50b20ec8ba462a13713ff1b18f6e85c448903d4a
kik <kik@1115ff72-a50f-0410-a26c-deacd0c0f9ad>
2013-12-11 09:03:47 +0000

    usuwanie starych plików
    git-svn-id: svn+ssh://szmaragd.im.pwr.wroc.pl/svn/RBO-new/JAVA@19541 1115ff72-a50f-0410-a26c-deacd0c0f9ad

--- skrypt-6.bash
+++
@@ -1,3 +0,0 @@
-for ((x=100; x<164; x++));do java MyXfig 6 ${x:1:2} > out-6-${x:1:2}.fig ;done
-for ((x=100; x<164; x++)); do fig2dev -L pdf  out-6-${x:1:2}.fig >out-6-${x:1:2}.pdf; done
-pdftk out-6-??.pdf cat output out-6.pdf
